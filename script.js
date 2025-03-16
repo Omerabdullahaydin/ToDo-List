dfddefb334929c538b8da6caea348fdb5ef64335
Ömer <mer@Aydn>
2025-03-16 18:20:27 +0300

    index.html

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let addButton = document.getElementById('addTodo')
+let addButton = document.getElementById('addTodo');
+let Container = document.getElementById('toDoContainer');
+let inputText = document.getElementById('inputText');
 
 addButton.addEventListener('click', function(){
     let addText = document.createElement('p');
-    
+    addText.classList.add('text');
+    addText.innerHTML = inputText.value
+    Container.appendChild(addText);
+
+    addText.addEventListener('click', function(){
+        addText.style.textDecoration = 'line-through';
+    })
+
 })
